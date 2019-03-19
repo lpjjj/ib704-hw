69e7c60de30ca59b9df8e3291310ce38fb75fd1c
lpjjj <48704436+lpjjj@users.noreply.github.com>
2019-03-19 15:46:07 +0800

    Add files via upload

---
+++ list-duplicate-number_lpj.py
@@ -0,0 +1,24 @@
+from collections import Counter
+def lpj (list) :
+    initial=0
+    list_index=[]
+    cal_n = Counter(list)
+    max = cal_n.most_common(1)
+    if max[0][1] > 1:
+        for n in range (list.count(max[0][0])) :
+            index=initial
+            a=max[0][0]
+            initial=list[initial:].index(a)
+            list_index.append(initial+index)
+            initial=list_index[-1:][0]+1
+        print(list_index)
+        return True
+    else :
+        return False
+
+# list_index=[1,3,5]
+# intial=list_index[-1:][0]
+# print(intial)
+
+assert lpj([4,6,3,2,7,9,7,7]) is True, fail
+assert lpj([4,6,3,2,7,9,5,1]) is False, fail
