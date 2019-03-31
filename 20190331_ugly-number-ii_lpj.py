eac94ecab25dec9cf2a76477181b295b49e559c1
lpjjj <48704436+lpjjj@users.noreply.github.com>
2019-04-01 00:28:48 +0800

    Add files via upload

---
+++ 20190331_ugly-number-ii_lpj.py
@@ -0,0 +1,21 @@
+def ugly2 (n):
+    order=[1]
+    none=[]
+    while pow(2, 31) > n and n != 0:
+        for i in range(1,n):
+
+            if i%2==0 and i%3==0 and i%5==0:
+                order.append(i)
+            elif (i%2==0 and i%3==0) or (i%2==0 and i%5==0) or (i%3==0 and i%5==0):
+                order.append(i)
+            elif (i%2==0 or i%3==0 or i%5==0) and i%7!=0:
+                    order.append(i)
+            else:
+                none.append(i)
+        return order
+    # order.index = range(1, len(order) + 1)
+    # return order.index(256)
+    # print(order.index(256))
+print(ugly2(260))
+
+
