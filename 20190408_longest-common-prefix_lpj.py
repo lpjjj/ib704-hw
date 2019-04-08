267157f8685344e05c7bf75dacf1cac80fe63b68
lpjjj <48704436+lpjjj@users.noreply.github.com>
2019-04-08 20:17:12 +0800

    Update 20190408_longest-common-prefix_lpj.py

--- 20190408_longest-common-prefix_lpj.py
+++ 20190408_longest-common-prefix_lpj.py
@@ -3,8 +3,7 @@ def common (str1,str2,str3) :
     str=[]
     for c in str1 :
         if c in str2 and c in str3 and str1.find(c)==str2.find(c)==str3.find(c) :
-            a=str1.find(c)
-            str.append(str1[a])
+            str.append(str1[str1.find(c)])
         else : pass
     return "".join(str)
 assert common("flower","flow","flight") == "fl",'fall'
