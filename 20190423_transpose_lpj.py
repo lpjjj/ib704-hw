3acc53cd7e72370281cafe54f2b828f8248271c4
lpjjj <48704436+lpjjj@users.noreply.github.com>
2019-04-24 01:36:46 +0800

    Create 20190423_transpose_lpj.py

---
+++ 20190423_transpose_lpj.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def transpose(list) :
+    matrix1=np.array(list)
+
+    b=len(matrix1[0])
+    matrix2 = []
+    for shape in range(b) :
+        matrix2.append(matrix1[:, shape])
+    matrix2=np.array(matrix2)
+    return matrix2
+    
+assert transpose([[1,2,5,7],[3,4,1,9],[5,6,8,7]]) == np.array([[1,2,5,7],[3,4,1,9],[5,6,8,7]]).T ,'fall'
